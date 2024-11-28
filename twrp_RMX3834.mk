66fd23fecfd79b318273d9b4c33f20852b437667
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-11-28 15:27:26 -0500

    RMX3834: Build PBRB

--- twrp_RMX3834.mk
+++ twrp_RMX3834.mk
@@ -19,7 +19,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product-if-exists, vendor/twrp/config/common.mk)
+
+# Inherit some common pb stuff.
+$(call inherit-product-if-exists, vendor/pb/config/common.mk)
 
 # Inherit from RMX3261 device
 $(call inherit-product, device/realme/RMX3834/device.mk)
